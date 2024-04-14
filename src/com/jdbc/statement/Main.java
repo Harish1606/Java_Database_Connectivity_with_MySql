a2bf0ffc2affbf5f05401e9db39033714e8c24cb
Harish <harishharish3759@gmail.com>
2024-04-14 16:08:31 +0530

    Updating the records using statement

--- src/com/jdbc/statement/Main.java
+++ src/com/jdbc/statement/Main.java
@@ -1,3 +1,5 @@
+package com.jdbc.statement;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -10,7 +12,8 @@ public class Main {
         String url = "jdbc:mysql://localhost:3306/userdb";
         String username = "root";
         String password = "Harish@1606";
-        String query = "SELECT * FROM employee;";
+        String query = "SELECT * FROM employee";
+        String updateQuery = "insert into employee values(7,'BE','harish@gmail.com','9309430930','harish')";
 
         Class.forName("com.mysql.cj.jdbc.Driver");
 
@@ -18,12 +21,18 @@ public class Main {
 
         Statement statement = connection.createStatement();
 
+        //executeQuery for fetching the records from the table
         ResultSet resultSet = statement.executeQuery(query);
 
         while (resultSet.next()) {
             String employeeData = resultSet.getInt(1) + " : " + resultSet.getString(2) + " : " + resultSet.getString(3) + " : " + resultSet.getString(4) + " : " + resultSet.getString(5);
             System.out.println(employeeData);
         }
+
+        //executeUpdate for updating the records in the table
+        int count = statement.executeUpdate(updateQuery);
+
+        System.out.println(count + " rows affected");
 
         statement.close();
         connection.close();
